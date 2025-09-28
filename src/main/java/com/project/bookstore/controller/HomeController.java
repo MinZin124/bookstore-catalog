81bd4a22a9d377781d8721c450331bd9569dd493
naraphumin <nara.phumin@gmail.com>
2025-09-28 13:07:45 +0700

    Access Current User

--- src/main/java/com/project/bookstore/controller/HomeController.java
+++ src/main/java/com/project/bookstore/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.project.bookstore.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +10,12 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "Welcome to home page";
+    }
+
+    @GetMapping("/whoami")
+    public String whoAmi() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username =auth.getName();
+        return "You are logged in as: " + username;
     }
 }
